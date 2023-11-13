abccf840b9d58d5865b42806eccf89ebfcbabb35
Oskar Lesment <oskar-aleksander.lesment@tptlive.ee>
2023-11-13 07:09:54 +0200

    13.11 Kasutaja vahendite lisamine. Commit enne database migratsiooni jms.

---
+++ KooliProjekt/Data/FundsTransaction.cs
@@ -0,0 +1,13 @@
+﻿namespace KooliProjekt.Data // 13.11 class for tracking funds movement
+{
+    public class FundsTransaction
+    {
+        public int FundsTransactionId { get; set; } 
+        public int FundID { get; set; } // Foreign key to the UserFunds
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string TransactionType { get; set; } // "Deposit" or "Withdrawal"
+        // Other relevant fields like TransactionDescription, etc.
+        public string? Comment { get; set; } // some comment if necessary for transaction, 
+    }
+}
---
+++ KooliProjekt/Data/UserFunds.cs
@@ -0,0 +1,15 @@
+﻿namespace KooliProjekt.Data
+{
+    public class UserFunds
+    {
+        //User UserID
+        public int FundID { get; set; }
+        public string? FundName { get; set; } = "Euros";// 13.11 praegu eurod
+        public decimal? Balance { get; set; } // vabad vahendid uuteks investeeringuteks v\i v2ljav6tmiseks. 
+        public decimal? DepositedFunds { get; set; } // platvormile kantud vahendid
+        public decimal? LockedFunds {  get; set; } //vahendid mison investeeringute all kinni
+       // public decimal? TotalFunds { get; set; } // vahendid kokku
+
+
+    }
+}
